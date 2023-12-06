8cf8ef0500f8478acd336116bdc50addd1a9b4ca
Sam Stewart <git@alt.ci>
2023-12-06 15:41:21 +0000

    Format `lettercoder` with `deno fmt`

--- src/decode.test.ts
+++ src/decode.test.ts
@@ -1,98 +1,98 @@
-import { assertEquals } from 'https://deno.land/std@0.208.0/assert/mod.ts';
-import { describe, it } from 'https://deno.land/std@0.208.0/testing/bdd.ts';
+import { assertEquals } from "https://deno.land/std@0.208.0/assert/mod.ts";
+import { describe, it } from "https://deno.land/std@0.208.0/testing/bdd.ts";
 
 import {
-  decodeQuotedPrintable,
-  decodeMimeWords,
   decodeMimeWord,
-} from './index.ts';
+  decodeMimeWords,
+  decodeQuotedPrintable,
+} from "./index.ts";
 
-describe('letterparser', () => {
-  it('decodes MIME word examples from section 8 of RFC 2047', () => {
-    assertEquals(decodeMimeWord('=?US-ASCII?Q?Keith_Moore?='), 'Keith Moore');
+describe("letterparser", () => {
+  it("decodes MIME word examples from section 8 of RFC 2047", () => {
+    assertEquals(decodeMimeWord("=?US-ASCII?Q?Keith_Moore?="), "Keith Moore");
 
     assertEquals(
-      decodeMimeWord('=?ISO-8859-1?Q?Keld_J=F8rn_Simonsen?='),
-      'Keld Jørn Simonsen'
+      decodeMimeWord("=?ISO-8859-1?Q?Keld_J=F8rn_Simonsen?="),
+      "Keld Jørn Simonsen",
     );
-    assertEquals(decodeMimeWord('=?ISO-8859-1?Q?Andr=E9?='), 'André');
+    assertEquals(decodeMimeWord("=?ISO-8859-1?Q?Andr=E9?="), "André");
     assertEquals(
-      decodeMimeWord('=?ISO-8859-1?B?SWYgeW91IGNhbiByZWFkIHRoaXMgeW8=?='),
-      'If you can read this yo'
-    );
-    assertEquals(
-      decodeMimeWord('=?ISO-8859-2?B?dSB1bmRlcnN0YW5kIHRoZSBleGFtcGxlLg==?='),
-      'u understand the example.'
+      decodeMimeWord("=?ISO-8859-1?B?SWYgeW91IGNhbiByZWFkIHRoaXMgeW8=?="),
+      "If you can read this yo",
     );
     assertEquals(
-      decodeMimeWord('=?ISO-8859-1?Q?Olle_J=E4rnefors?='),
-      'Olle Järnefors'
+      decodeMimeWord("=?ISO-8859-2?B?dSB1bmRlcnN0YW5kIHRoZSBleGFtcGxlLg==?="),
+      "u understand the example.",
     );
     assertEquals(
-      decodeMimeWord('=?ISO-8859-1?Q?Patrik_F=E4ltstr=F6m?='),
-      'Patrik Fältström'
+      decodeMimeWord("=?ISO-8859-1?Q?Olle_J=E4rnefors?="),
+      "Olle Järnefors",
     );
     assertEquals(
-      decodeMimeWord('=?iso-8859-8?b?7eXs+SDv4SDp7Oj08A==?='),
-      'םולש ןב ילטפנ'
+      decodeMimeWord("=?ISO-8859-1?Q?Patrik_F=E4ltstr=F6m?="),
+      "Patrik Fältström",
+    );
+    assertEquals(
+      decodeMimeWord("=?iso-8859-8?b?7eXs+SDv4SDp7Oj08A==?="),
+      "םולש ןב ילטפנ",
     );
   });
 
-  it('decodes Quoted-Printable', () => {
+  it("decodes Quoted-Printable", () => {
     assertEquals(
       decodeQuotedPrintable(
         "Now's the time =\nfor all folk to come=\n to the aid of their country.",
-        'utf8'
+        "utf8",
       ),
-      "Now's the time for all folk to come to the aid of their country."
+      "Now's the time for all folk to come to the aid of their country.",
     );
 
     assertEquals(
       decodeQuotedPrintable(
         "J'interdis aux marchands de vanter trop leurs marchandises. Car ils se font =\nvite p=C3=A9dagogues et t'enseignent comme but ce qui n'est par essence qu'=\nun moyen, et te trompant ainsi sur la route =C3=A0 suivre les voil=C3=A0 bi=\nent=C3=B4t qui te d=C3=A9gradent, car si leur musique est vulgaire ils te f=\nabriquent pour te la vendre une =C3=A2me vulgaire.",
-        'utf8'
+        "utf8",
       ),
-      "J'interdis aux marchands de vanter trop leurs marchandises. Car ils se font vite pédagogues et t'enseignent comme but ce qui n'est par essence qu'un moyen, et te trompant ainsi sur la route à suivre les voilà bientôt qui te dégradent, car si leur musique est vulgaire ils te fabriquent pour te la vendre une âme vulgaire."
+      "J'interdis aux marchands de vanter trop leurs marchandises. Car ils se font vite pédagogues et t'enseignent comme but ce qui n'est par essence qu'un moyen, et te trompant ainsi sur la route à suivre les voilà bientôt qui te dégradent, car si leur musique est vulgaire ils te fabriquent pour te la vendre une âme vulgaire.",
     );
 
-    assertEquals(decodeQuotedPrintable('=F0=9F=91=8D', 'utf8'), '👍');
+    assertEquals(decodeQuotedPrintable("=F0=9F=91=8D", "utf8"), "👍");
 
     assertEquals(
-      decodeQuotedPrintable('=DE=AD=BE=EF'),
-      Uint8Array.from([0xde, 0xad, 0xbe, 0xef])
+      decodeQuotedPrintable("=DE=AD=BE=EF"),
+      Uint8Array.from([0xde, 0xad, 0xbe, 0xef]),
     );
 
-    assertEquals(decodeQuotedPrintable('____', 'utf8'), '____');
+    assertEquals(decodeQuotedPrintable("____", "utf8"), "____");
   });
 
-  it('handles multiple mime words properly', () => {
-    assertEquals(decodeMimeWords('=?ISO-8859-1?Q?a?= b'), 'a b');
-    assertEquals(decodeMimeWords('a =?ISO-8859-1?Q?a?= b'), 'a a b');
+  it("handles multiple mime words properly", () => {
+    assertEquals(decodeMimeWords("=?ISO-8859-1?Q?a?= b"), "a b");
+    assertEquals(decodeMimeWords("a =?ISO-8859-1?Q?a?= b"), "a a b");
     assertEquals(
-      decodeMimeWords('=?ISO-8859-1?Q?a?= =?ISO-8859-1?Q?b?='),
-      'ab'
+      decodeMimeWords("=?ISO-8859-1?Q?a?= =?ISO-8859-1?Q?b?="),
+      "ab",
     );
     assertEquals(
-      decodeMimeWords('=?ISO-8859-1?Q?a?=  =?ISO-8859-1?Q?b?='),
-      'ab'
+      decodeMimeWords("=?ISO-8859-1?Q?a?=  =?ISO-8859-1?Q?b?="),
+      "ab",
     );
     assertEquals(
-      decodeMimeWords('=?ISO-8859-1?Q?a?=\n =?ISO-8859-1?Q?b?='),
-      'ab'
+      decodeMimeWords("=?ISO-8859-1?Q?a?=\n =?ISO-8859-1?Q?b?="),
+      "ab",
     );
     assertEquals(
       decodeMimeWords(
-        '=?ISO-8859-1?B?SWYgeW91IGNhbiByZWFkIHRoaXMgeW8=?= =?ISO-8859-2?B?dSB1bmRlcnN0YW5kIHRoZSBleGFtcGxlLg==?='
+        "=?ISO-8859-1?B?SWYgeW91IGNhbiByZWFkIHRoaXMgeW8=?= =?ISO-8859-2?B?dSB1bmRlcnN0YW5kIHRoZSBleGFtcGxlLg==?=",
       ),
-      'If you can read this you understand the example.'
+      "If you can read this you understand the example.",
     );
 
-    assertEquals(decodeMimeWords('=?ISO-8859-1?Q?a_b_c?= b'), 'a b c b');
+    assertEquals(decodeMimeWords("=?ISO-8859-1?Q?a_b_c?= b"), "a b c b");
   });
 
-  it('handles the lack of mime words properly', () => {
-    assertEquals(decodeMimeWords('a b'), 'a b');
-    assertEquals(decodeMimeWords('a@b.com'), 'a@b.com');
-    assertEquals(decodeMimeWords('text/plain'), 'text/plain');
+  it("handles the lack of mime words properly", () => {
+    assertEquals(decodeMimeWords("a b"), "a b");
+    assertEquals(decodeMimeWords("a@b.com"), "a@b.com");
+    assertEquals(decodeMimeWords("text/plain"), "text/plain");
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,35 @@
-import { decodeBase64 as toByteArray } from 'https://deno.land/std@0.208.0/encoding/base64.ts';
+import { decodeBase64 as toByteArray } from "https://deno.land/std@0.208.0/encoding/base64.ts";
 
-if (typeof TextDecoder === 'undefined') {
+if (typeof TextDecoder === "undefined") {
   // @ts-ignore Isomorphism (Deno-specific).
-  const nodeVer = typeof process !== 'undefined' && process.versions?.node;
+  const nodeVer = typeof process !== "undefined" && process.versions?.node;
   const nodeRequire = nodeVer
-    ? // @ts-ignore Isomorphism.
-      typeof __webpack_require__ === 'function'
-      ? // @ts-ignore Isomorphism.
-        __non_webpack_require__
+    // @ts-ignore Isomorphism.
+    ? typeof __webpack_require__ === "function"
+      // @ts-ignore Isomorphism.
+      ? __non_webpack_require__
       // @ts-ignore Isomorphism (Deno-specific).
       : require
     : undefined;
   // @ts-ignore Isomorphism.
-  global['TextDecoder'] = nodeRequire('util').TextDecoder;
+  global["TextDecoder"] = nodeRequire("util").TextDecoder;
 }
 
 const CHAR_SPACE = 0x20;
 
 function decodeQAndQP(input: string, encoding?: string, isQ = false) {
-  const lines = input.replace(/\r/g, '').split('\n');
-  let tempStr = '';
+  const lines = input.replace(/\r/g, "").split("\n");
+  let tempStr = "";
 
   for (let line of lines) {
-    if (line.endsWith('=')) {
+    if (line.endsWith("=")) {
       tempStr += line.substring(0, line.length - 1);
     } else {
-      tempStr += line + '\n';
+      tempStr += line + "\n";
     }
   }
 
-  if (tempStr.endsWith('\n')) {
+  if (tempStr.endsWith("\n")) {
     tempStr = tempStr.substring(0, tempStr.length - 1);
   }
 
@@ -39,7 +39,7 @@ function decodeQAndQP(input: string, encoding?: string, isQ = false) {
     const charCode = tempStr.charCodeAt(i);
 
     switch (char) {
-      case '=':
+      case "=":
         const hex = parseInt(tempStr.charAt(i + 1) + tempStr.charAt(i + 2), 16);
         if (hex) {
           bytes.push(hex);
@@ -48,7 +48,7 @@ function decodeQAndQP(input: string, encoding?: string, isQ = false) {
           bytes.push(charCode);
         }
         break;
-      case '_':
+      case "_":
         bytes.push(isQ ? CHAR_SPACE : charCode);
         break;
       default:
@@ -70,21 +70,21 @@ function decodeQAndQP(input: string, encoding?: string, isQ = false) {
  * @param input
  */
 export function decodeMimeWords(input: string) {
-  const atoms = input.replace(/\s+/g, ' ').split(' ');
-  let output = '';
+  const atoms = input.replace(/\s+/g, " ").split(" ");
+  let output = "";
   let wasMimeWord = false;
 
   for (let atom of atoms) {
     if (isMimeWord(atom)) {
-      if (!wasMimeWord && output !== '') {
-        output += ' ';
+      if (!wasMimeWord && output !== "") {
+        output += " ";
       }
       output += decodeMimeWord(atom);
 
       wasMimeWord = true;
     } else {
-      if (output !== '') {
-        output += ' ' + atom.trim();
+      if (output !== "") {
+        output += " " + atom.trim();
       } else {
         output += atom.trim();
       }
@@ -97,17 +97,17 @@ export function decodeMimeWords(input: string) {
 }
 
 export function isMimeWord(input: string) {
-  if (!input.startsWith('=?') || !input.endsWith('?=') || input.includes(' ')) {
+  if (!input.startsWith("=?") || !input.endsWith("?=") || input.includes(" ")) {
     return false;
   }
 
-  const split = input.split('?');
+  const split = input.split("?");
   if (split.length !== 5) {
     return false;
   }
 
   const type = split[2].toLowerCase();
-  return type === 'q' || type === 'b';
+  return type === "q" || type === "b";
 }
 
 /**
@@ -119,14 +119,14 @@ export function decodeMimeWord(input: string) {
     return input;
   }
 
-  let [encoding, type, value] = input.split('?').slice(1, 4);
+  let [encoding, type, value] = input.split("?").slice(1, 4);
 
   // Remove language tag.
-  encoding = encoding.split('*')[0].toLowerCase();
+  encoding = encoding.split("*")[0].toLowerCase();
   type = type.toLowerCase();
 
   switch (type) {
-    case 'b':
+    case "b":
       const bytes = toByteArray(value);
       if (bytes) {
         const decoder = new TextDecoder(encoding);
@@ -134,7 +134,7 @@ export function decodeMimeWord(input: string) {
       } else {
         return input;
       }
-    case 'q':
+    case "q":
       return decodeQAndQP(value, encoding, true);
     default:
       return input;
